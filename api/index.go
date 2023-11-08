4d2df72d6ff24e3ab2b0e87409a32c83402f458e
Yuta Yoshimura <yuta_yoshimura@cyberagent.co.jp>
2023-11-08 11:06:53 +0900

    WIP

--- api/index.go
+++ api/index.go
@@ -1,12 +1,37 @@
 package handler
 
 import (
+	"io"
 	"net/http"
 	"os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fileBytes, err := os.ReadFile("../src/sampale.jpeg")
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fileSrc, _, err := r.FormFile("upload")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer fileSrc.Close()
+
+	fileDest, err := os.Create("/tmp/original.jpg")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer fileDest.Close()
+
+	if _, err := io.Copy(fileDest, fileSrc); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fileBytes, err := os.ReadFile(fileDest.Name())
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
